fb6094f0ce02e340ff7e7df7c685f4b3863c5e8d
Дмитрий Китаев <idems.91@gmail.com>
2018-07-01 18:08:16 +0700

    girls retina

--- js/main.js
+++ js/main.js
@@ -178,6 +178,22 @@ $(document).ready(function(){
         }
     });
 
+    isRetina = retina();
+
+    function retina(){
+        var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
+            (min--moz-device-pixel-ratio: 1.5),\
+            (-o-min-device-pixel-ratio: 3/2),\
+            (min-resolution: 1.5dppx)";
+        if (window.devicePixelRatio > 1)
+            return true;
+        if (window.matchMedia && window.matchMedia(mediaQuery).matches)
+            return true;
+        return false;
+    }
+
+    
+
     // $("body").children().each(function() {
     //     $(this).html($(this).html().replace(/&#8232;/g," "));
     // });
